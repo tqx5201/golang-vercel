cf31e223d56d131b818e6af6e440d27d8aa68c78
天仙婆婆 <tqx5201@163.com>
2024-08-01 20:41:39 +0800

    Update routes.go

--- app/routes/routes.go
+++ app/routes/routes.go
@@ -17,7 +17,7 @@ func Register(app *gin.Engine) {
 	app.NoRoute(ErrRouter)
 
 	app.GET("/ping", handler.Ping)
-        app.GET("/:path/:rid", handler.feiyang)
+        app.GET("/:path/:rid", handler.Feiyang)
 	route := app.Group("/api")
 	{
 		route.GET("/hello/:name", handler.Hello)
