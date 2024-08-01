116f8c1d89df5a037eaaf1bd456bb487b3e40790
天仙婆婆 <tqx5201@163.com>
2024-08-01 11:02:35 +0800

    Update itv.go

--- app/liveurls/itv.go
+++ app/liveurls/itv.go
@@ -237,7 +237,7 @@ func (i *Itv) HandleMainRequest(c *gin.Context, cdn, id string) {
 		return
 	}
 	redirectPrefix := redirectURL[:strings.LastIndex(redirectURL, "/")+1]
-
+c.String(http.StatusOK, redirectPrefix)	
 	// 替换TS文件的链接
 	golang := "http://" + c.Request.Host + c.Request.URL.Path
 	re := regexp.MustCompile(`((?i).*?\.ts)`)
